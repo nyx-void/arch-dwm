7756bc8ac15f83f0f8a254ba1304cacb4abea314
nyx-void <amrit44404@proton.me>
2024-09-27 16:49:11 +0545

    added zed

--- dwm/config.h
+++ dwm/config.h
@@ -5,6 +5,7 @@
 #define TERMCLASS "St"
 #define BROWSER "firefox"
 #define BIT "bleachbit"
+#define ZED "zeditor"
 
 /* appearance */
 static unsigned int borderpx  = 3;        // border pixel of windows
@@ -160,6 +161,7 @@ static const Key keys[] = {
 	{ MODKEY,			XK_w,		spawn,		{.v = (const char*[]){ BROWSER, NULL } } },
 	{ MODKEY|ShiftMask,		XK_w,		spawn,		{.v = (const char*[]){ TERMINAL, "-e", "nmtui", NULL } } },
 	{ MODKEY|ShiftMask,		XK_b,		spawn,		{.v = (const char*[]){ BIT, NULL } } },
+	{ MODKEY,			XK_e,		spawn,		{.v = (const char*[]){ ZED, NULL } } },
 	{ MODKEY,			XK_p,		spawn,		SHCMD(TERMINAL " -e alsamixer ") },
 	{ MODKEY,			XK_Escape,	spawn,		SHCMD(TERMINAL " -e htop") },
 	//{ MODKEY|ShiftMask,		XK_backslash,	spawn,		{.v = (const char*[]){ TERMINAL, "-e", "lf", NULL } } },
