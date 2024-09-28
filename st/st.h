dd92b37c5a43d80a6fcf852e7a75733fe8646538
nyx-void <amrit44404@proton.me>
2024-09-28 19:04:36 +0545

    open dir in CWD

--- st/st.h
+++ st/st.h
@@ -89,6 +89,7 @@ void externalpipe(const Arg *);
 void kscrolldown(const Arg *);
 void kscrollup(const Arg *);
 
+void newterm(const Arg *);
 void printscreen(const Arg *);
 void printsel(const Arg *);
 void sendbreak(const Arg *);
